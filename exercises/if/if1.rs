c1acf7a989148b9d78844882d670c744098a39ba
guyuelun <gongchaosheng2020@163.com>
2022-11-07 15:44:43 +0800

    update

--- exercises/if/if1.rs
+++ exercises/if/if1.rs
@@ -8,6 +8,15 @@ pub fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+    if a > b
+    {
+        b
+    }else
+    {
+        a
+    }
+        
+   
 }
 
 // Don't mind this for now :)
